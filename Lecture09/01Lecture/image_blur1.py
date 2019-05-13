f4b011d8c4225967898219882a37489bf7d6a3d0
Changbeom Choi <cbchoi@handong.edu>
2019-05-13 14:48:31 +0900

    Lecture File Update

--- Lecture09/01Lecture/image_blur1.py
+++
@@ -1,9 +0,0 @@
-import cv2
-img = cv2.imread('pikachu1.png', cv2.IMREAD_COLOR)
-cv2.imshow('before', img)
-
-dst = cv2.GaussianBlur(img, (5,5), 0)
-cv2.imshow('after', dst)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
